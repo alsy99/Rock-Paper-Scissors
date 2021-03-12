45a283ff5c4340cc2384eb474e1db34968ea202c
alsy99 <shahid.adnan99@gmail.com>
2021-03-12 21:37:41 +0530

    Create Rock_Paper_Scissors.py
    Source code

---
+++ Rock_Paper_Scissors.py
@@ -0,0 +1,28 @@
+import random as r
+
+
+def play():
+    user = input("""'R' for Rock
+'P' for Paper
+'S' for Scissors
+
+Your Choice:""").lower()
+    com = r.choice(['r', 'p', 's'])
+
+    choices = {'r':0, 's':1, 'p':2}
+
+    if user not in choices.keys():
+        print("Enter valid choice!")
+        exit()
+
+    print(f'\nComputer chose: {com.upper()}\n')
+
+    if (choices.get(user) - choices.get(com)) % 3 == 2:
+        print('You Won!')
+    elif (choices.get(user) - choices.get(com)) % 3 == 1:
+        print('You Lost!')
+    else:
+        print("It's a Tie!")
+
+
+play()
